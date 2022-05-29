6b6dce0655a53552db832793ddcc18332d58f9f2
PhoenixPlanet <ytw0113@gmail.com>
2022-05-29 22:45:05 +0900

    refactor: Change Button class to function component

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Styles from "./Button.module.css"
 
-class Button extends React.Component {
-    render() {
-        return (
-            <div className={Styles.Button}>
-                {this.props.children}
-            </div>
-        );
-    } 
+function Button(props) {
+    return (
+        <div className={Styles.Button}>
+            {props.children}
+        </div>
+    );
 }
 
 export default Button;
