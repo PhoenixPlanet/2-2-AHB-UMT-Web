40cfd7f4a24b0b2e2594959a675f5f47065fe258
PhoenixPlanet <ytw0113@gmail.com>
2022-06-04 17:39:15 +0900

    feat: Add Card component

---
+++ src/Components/UI/Card.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styles from "./Card.module.css"
+
+function Card(props) {
+    return (
+        <div className={Styles.Card}>
+            
+        </div>
+    );
+}
+
+export default Card;
