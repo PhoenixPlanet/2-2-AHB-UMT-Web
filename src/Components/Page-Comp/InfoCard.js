23bb3612798cdcb0cec5282364b4db4d73018391
PhoenixPlanet <ytw0113@gmail.com>
2022-06-11 23:45:01 +0900

    feat: Design InfoCard layout

--- src/Components/Page-Comp/InfoCard.js
+++ src/Components/Page-Comp/InfoCard.js
@@ -1,16 +1,44 @@
 import React from "react";
 import Styles from "./InfoCard.module.css"
+import styled, { css } from "styled-components"
 
 import Card from "../UI/Card";
+import Button from "../UI/Button"
+
+const InfoCardLayout = css`
+	/* layout settings */
+	display: grid;
+	grid-template-rows: 150px minmax(50px, auto) minmax(150px, auto) minmax(40px, auto);
+	overflow: hidden;
+`
+
+const Gap = styled.br`
+	display: block;
+	margin-top: 5px;
+	content: " ";
+`
 
 function InfoCard(props) {
-	
     return (
-        <Card cardLayout={Styles.InfoCard}>
+        <Card cardLayout={InfoCardLayout}>
 			<img className={Styles.Image} src="image/k16umt.jpg" alt="K16 UMT"/>
 			<div className={Styles.Title}>2-2 UMT</div>
-			<div className={Styles.Info}></div>
-			<div className={Styles.Actions}></div>
+			<div className={Styles.Info}>
+				2-2 Unit Ministry team
+				<Gap/>
+				<b>Office Call(DSN)</b> 
+				<br/>
+				722-8508
+				<Gap/>
+				<b>Office Call(Comm)</b>
+				<br/>
+				0503-322-8508
+			</div>
+			<div className={Styles.Actions}>
+				<Button>
+					Call
+				</Button>
+			</div>
 		</Card>
     );
 }
