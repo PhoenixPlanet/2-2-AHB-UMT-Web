d00205c615917a53af61f2f6478aceda398a71d5
PhoenixPlanet <ytw0113@gmail.com>
2022-06-11 23:44:11 +0900

    refactor: Change css module to 'css in js'

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -1,11 +1,39 @@
 import React from "react";
-import Styles from "./Card.module.css"
+import styled from "styled-components"
 
-function Card({children, cardLayout, cardStyle}) {
+const StyledCard = styled.div`
+    /* layout settings */
+    display: grid;
+
+    width: 250px;
+    min-height: 350px;
+
+    margin: 20px;
+
+    ${(props) => props.cardLayout};
+
+    /* font settings */
+
+    /* design */
+    background-color: white;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.2), 0.5px 0 5px rgba(0, 0, 0, 0.1);
+
+    border-radius: 10px;
+        
+    /* animation */
+    transition: box-shadow 0.2s ease;
+
+    /* extra */
+    &:hover {
+        box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.2), 0.5px 0 5px rgba(0, 0, 0, 0.1);
+    }
+`;
+
+function Card({children, cardLayout, ...cardStyle}) {
     return (
-        <div className={`${Styles.Card} ${cardLayout}`}>
+        <StyledCard cardLayout={cardLayout} cardStyle={cardStyle}>
             {children}
-        </div>
+        </StyledCard>   
     );
 }
 
