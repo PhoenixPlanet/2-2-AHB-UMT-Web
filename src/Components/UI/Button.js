aa2d85b7844837d996848a0c240c24957c7060c6
PhoenixPlanet <ytw0113@gmail.com>
2022-06-12 17:38:30 +0900

    refactor: Change css module to css in js in Button Component

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,11 +1,55 @@
 import React from "react";
-import Styles from "./Button.module.css"
+import styled from "styled-components"
 
-function Button(props) {
+const StyledButton = styled.div`
+    all:unset;
+    
+	/* layout settings */
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	
+    width: 6rem;
+    height: 2rem;
+	
+    padding: 0px;
+    margin: 10px;
+	
+	/* font settings */
+    font-size: 1.3rem;
+    /* line-height: 1.2rem; */
+	/* font-family: 'GmarketSansMedium'; */
+	
+	/* design */
+	color: ${ props => props.style.color || "#41403E" };
+    border: 2px solid ${ props => props.style.color || "#41403E" };
+    border-radius: 0.7rem;
+	/* color: rgba(var(--a), 1); */
+    /* border: 2px solid rgba(var(--a), 1); */
+    /* border-radius: 5px; */
+	    
+	/* animation */
+    transition: background-color 0.3s ease, border-radius 0.3s ease;
+	
+	/* extra */
+    cursor: pointer;
+
+    &:hover {
+        background-color: ${ props => props.style.color || "#41403E" };
+        border-radius: 35px 225px;
+        color: white;
+    }
+`
+
+function Button({ children, style = {}, onClickEvent }) {
+    function onClick() {
+        onClickEvent();
+    }
+
     return (
-        <div className={Styles.Button}>
-            {props.children}
-        </div>
+        <StyledButton onClick={onClick} style={style}>
+            {children}
+        </StyledButton>
     );
 }
 
